3fec5a25b4472b5ddcd1a5900d8e37dd66ab39b4
Inzamul Akib <77077129+inaaki@users.noreply.github.com>
2023-04-14 21:46:07 +0600

    feat: add multiple url scraping support

--- main.py
+++ main.py
@@ -10,21 +10,32 @@ def is_searched(soup_img, terms) -> bool:
     return any(word in target for word in terms)
 
 
-search_tags = ['coffee', 'blue', 'sky', 'flower', 'computer', 'table']
-url = 'https://unsplash.com/'
+SEARCH_TAGS = ['coffee', 'blue', 'sky', 'flower', 'computer', 'table']
+URLS = ['https://unsplash.com/', 'https://www.shutterstock.com/images']
 
-with urlopen(url) as page:
-    page_content = page.read()
 
-# create soup container
-soup = BeautifulSoup(page_content, 'html.parser')
-images = soup.findAll('img')
+def print_image_links(url, search_terms):
 
-# images that contain any word of "search tags"
-filtered_image = [img for img in images if is_searched(img, search_tags)]
+    with urlopen(url) as page:
+        page_content = page.read()
 
-# extracting links from filtered images
-img_links = [img.get('src') for img in filtered_image if img.get('src')]
+    # create soup container
+    soup = BeautifulSoup(page_content, 'html.parser')
+    images = soup.findAll('img')
 
-for link in img_links:
-    print(link)
+    # filtering based on "search tags"
+    filtered_image = [img for img in images if is_searched(img, search_terms)]
+
+    # extracting links from filtered images
+    img_links = [img.get('src') for img in filtered_image if img.get('src')]
+
+    if img_links:
+        for link in img_links:
+            print(f'\nPhotos from {url}')
+            print(link)
+    else:
+        print(f"\nNo Photos found on {url}\n")
+
+
+for url in URLS:
+    print_image_links(url, SEARCH_TAGS)
