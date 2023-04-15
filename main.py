6125f3f8c1e71b47ac1bc82c8360efb48a9cd379
Inzamul Akib <77077129+inaaki@users.noreply.github.com>
2023-04-15 14:34:25 +0600

    fix(src): find image source based on src hierarchy

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ from urllib.request import urlopen, Request
 import sys
 
 
+def get_image_source(soup_img):
+    # return image source based on src hierarchy
+    try:
+        src = soup_img['data-srcset']
+    except:
+        try:
+            src = soup_img['data-src']
+        except:
+            try:
+                src = soup_img['data-fallback-src']
+            except:
+                try:
+                    src = soup_img['src']
+                except:
+                    src = ''
+    return src
+
+
 def is_searched(soup_img, terms) -> bool:
     # boolean check for "search tags" in a distinct image container
-    src = soup_img.get('src')
+    src = get_image_source(soup_img)
     alt = soup_img.get('alt')
     target = ' '.join(filter(None, [src, alt]))
     return any(word in target for word in terms)
@@ -40,7 +58,7 @@ def print_image_links(url, search_terms):
     filtered_image = [img for img in images if is_searched(img, search_terms)]
 
     # extracting links from filtered images
-    img_links = [define_path(url, img.get('src'))
+    img_links = [define_path(url, get_image_source(img))
                  for img in filtered_image if img.get('src')]
 
     if img_links:
